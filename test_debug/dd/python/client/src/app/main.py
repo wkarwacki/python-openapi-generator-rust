58e96e0b1f81ff6074752c911fad25eda89c3efb
wkarwacki <wojciech.karwacki@dotdata.com>
2024-07-11 22:36:42 +0200

    removed test-debug dir

--- test_debug/dd/python/client/src/app/main.py
+++
@@ -1,8 +0,0 @@
-from trust.dev.service import DevService
-from trust.table.service import TableService
-
-dev_service = DevService()
-table_service = TableService()
-
-print(dev_service.get_health())
-print(table_service.get_tables())
--- test_debug/dd2/python/client/src/app/main.py
+++
@@ -1,5 +0,0 @@
-from trust.dev.service import DevService
-
-dev_service = DevService()
-
-print(dev_service.get_health())
