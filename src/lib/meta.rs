b5abea58ab7bbc84866220243a418c8846759966
wkarwacki <wojciech.karwacki@dotdata.com>
2024-07-19 12:08:20 +0200

    use default macro for meta

--- src/lib/meta.rs
+++ src/lib/meta.rs
@@ -2,18 +2,10 @@ use crate::lib::desc::Desc;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 pub(crate) struct Meta {
     #[serde(flatten)]
     pub value: HashMap<String, Desc>,
-}
-
-impl Default for Meta {
-    fn default() -> Self {
-        Meta {
-            value: HashMap::default(),
-        }
-    }
 }
 
 impl Meta {
