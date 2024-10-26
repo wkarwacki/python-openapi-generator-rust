2d91e1b63399afff32438573c2c8f29ec4a6d00a
wkarwacki <wojciech.karwacki@dotdata.com>
2024-10-26 17:44:18 +0200

    release TrustSpecCli

--- release.sh
+++ release.sh
@@ -5,24 +5,7 @@ set -eox pipefail
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cd "$SCRIPT_DIR" || exit
 
-VERSION=$(yq -oy '.package.version' Cargo.toml)
-
-trap "mv $SCRIPT_DIR/plugin/py-binding/Cargo.toml.bu $SCRIPT_DIR/plugin/py-binding/Cargo.toml; mv $SCRIPT_DIR/cli/py/pyproject.toml.bu $SCRIPT_DIR/cli/py/pyproject.toml" EXIT
-
-cp $SCRIPT_DIR/plugin/py-binding/Cargo.toml $SCRIPT_DIR/plugin/py-binding/Cargo.toml.bu
-sed -i "s/{VERSION}/$VERSION/g" $SCRIPT_DIR/plugin/py-binding/Cargo.toml
-
-cp $SCRIPT_DIR/cli/py/pyproject.toml $SCRIPT_DIR/cli/py/pyproject.toml.bu
-sed -i "s/{VERSION}/$VERSION/g" $SCRIPT_DIR/cli/py/pyproject.toml
-
-bash do_test.sh
-
-cd plugin/py-binding
-bash build.sh
-maturin upload target/wheels/*
-
-cd $SCRIPT_DIR/cli/py
-twine upload dist/*
+bash do_release.sh
 
 cd $SCRIPT_DIR
 git add .
