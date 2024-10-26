18d29885f2a713d3e6f4123c6a906b4d01a29e53
wkarwacki <wojciech.karwacki@dotdata.com>
2024-10-26 17:51:16 +0200

    release TrustSpecCli 0.1.26

--- do_release.sh
+++ do_release.sh
@@ -5,8 +5,6 @@ set -eox pipefail
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cd "$SCRIPT_DIR" || exit
 
-VERSION=$(yq -oy '.package.version' Cargo.toml)
-
 trap "mv $SCRIPT_DIR/plugin/py-binding/Cargo.toml.bu $SCRIPT_DIR/plugin/py-binding/Cargo.toml; mv $SCRIPT_DIR/cli/py/pyproject.toml.bu $SCRIPT_DIR/cli/py/pyproject.toml" EXIT
 
 cp $SCRIPT_DIR/plugin/py-binding/Cargo.toml $SCRIPT_DIR/plugin/py-binding/Cargo.toml.bu
--- release.sh
+++ release.sh
@@ -5,6 +5,8 @@ set -eox pipefail
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cd "$SCRIPT_DIR" || exit
 
+export VERSION=$(yq -oy '.package.version' Cargo.toml)
+
 bash do_release.sh
 
 cd $SCRIPT_DIR
